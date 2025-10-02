408c0c0a693126f61ae97d90978c3c2080e7722c
SAYOOJ KS <118405061+sayooj100@users.noreply.github.com>
2025-10-02 19:38:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
 from flask import Flask
-import threading
-import bot_main  # Import your bot file (bot_main.py)
-
 app = Flask(__name__)
 
-# Health check endpoint for Koyeb
-@app.route("/")
-def health():
-    return "OK", 200
+@app.route('/')
+def hello_world():
+    return 'GreyMatters'
 
-# Start the bot in a separate thread
-def start_bot():
-    bot_main.infinity_polling()
 
 if __name__ == "__main__":
-    threading.Thread(target=start_bot, daemon=True).start()
-    # Run Flask for health check
-    app.run(host="0.0.0.0", port=8080)
+    app.run()
+
 
 
