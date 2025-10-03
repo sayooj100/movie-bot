b05cbf4d5b38274cb9c4bf55a42fb11afc9d6a53
SAYOOJ KS <118405061+sayooj100@users.noreply.github.com>
2025-10-03 21:30:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask
-import threading
-import bot_main  # your bot code
 
 app = Flask(__name__)
 
@@ -8,11 +6,9 @@ app = Flask(__name__)
 def health():
     return "OK", 200
 
-def run_bot():
-    bot_main.infinity_polling()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
 
-# Start the bot in a separate thread
-threading.Thread(target=run_bot, daemon=True).start()
 
 
 
